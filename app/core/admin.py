d97391024ed8d15d27cf9e93dd430ad1b6a64b23
ogabrielluiz <byllbi@gmail.com>
2019-11-04 07:52:55 -0200

    Update admin to support custom user model

--- app/core/admin.py
+++ app/core/admin.py
@@ -15,6 +15,12 @@ class UserAdmin(BaseUserAdmin):
             "fields": ("is_active", "is_staff", "is_superuser",)}),
         (_("Important dates"), {"fields": ("last_login",)}),
     )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password')
+            }),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
